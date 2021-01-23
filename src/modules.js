5d8ab877af7648aee294d9e5ff82e7da291bbeb1
Oj <oj@oojmed.com>
2021-01-23 10:09:29 +0000

    Add new module repo

--- src/modules.js
+++ src/modules.js
@@ -1,4 +1,5 @@
 export default
   [
-    'GooseMod/GMExampleModule@7848c0986bb30c2afac9e77e191bd172a2d02f73'
+    'GooseMod/GMExampleModule@7848c0986bb30c2afac9e77e191bd172a2d02f73',
+    'Controlfreak707/GM-Copy-Raw@f44862f626700f2644325301acea94d130f9c115'
   ];
