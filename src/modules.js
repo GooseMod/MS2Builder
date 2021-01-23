fc82d590e48035c93a901a95e7af2a18a402c86c
Oj <oj@oojmed.com>
2021-01-23 10:22:08 +0000

    Bump commit for GM-Copy-Raw

--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,5 @@
 export default
   [
     'GooseMod/GMExampleModule@7848c0986bb30c2afac9e77e191bd172a2d02f73',
-    'Controlfreak707/GM-Copy-Raw@f44862f626700f2644325301acea94d130f9c115'
+    'Controlfreak707/GM-Copy-Raw@ae3f739f4d53344f87b1b404686d100394a566ee'
   ];
