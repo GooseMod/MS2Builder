f518a10cfe87a2e0f3e7fa1fe64bf1ba8d0338c0
Oj <oj@oojmed.com>
2021-01-23 10:47:48 +0000

    Bump GMExampleModule commit

--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,5 @@
 export default
   [
-    'GooseMod/GMExampleModule@7848c0986bb30c2afac9e77e191bd172a2d02f73',
+    'GooseMod/GMExampleModule@f4cec6c0d5f9632a44de907d647f3ad02d03bb3f',
     'Controlfreak707/GM-Copy-Raw@ae3f739f4d53344f87b1b404686d100394a566ee'
   ];
