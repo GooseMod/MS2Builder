ecc892110b86f9635269f5aec8998f2cfe8c7e30
Oj <oj@oojmed.com>
2021-02-06 20:13:01 +0000

    [Modules] Update modules JS (2)

--- src/modules/goosemod.js
+++ src/modules/goosemod.js
@@ -3,7 +3,7 @@ export default
   ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d'],
   ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d', '/subModule'],
 
-  ['Controlfreak707/GM-Copy-Raw', 'ae3f739f4d53344f87b1b404686d100394a566ee'],
+  ['Controlfreak707/GM-Copy-Raw', '91a235e09bbb3168c67bc535bdab2a03e88b2a98'],
   ['Controlfreak707/GM-Send-Button', '0c326a4c0968a527fbbc776682b730a83b27e628'],
   ['Controlfreak707/GM-Relaunch', '75c838c352743becdef0d68a954cb5d4f43bce5b']
 ];
