cd4b6a733364aad4c183881896760bca857f681c
Oj <oj@oojmed.com>
2021-02-07 15:54:19 +0000

    [Modules] Remove example modules

--- src/modules/goosemod.js
+++ src/modules/goosemod.js
@@ -1,7 +1,7 @@
 export default
 [
-  ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d'],
-  ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d', '/subModule'],
+  // ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d'],
+  // ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d', '/subModule'],
 
   ['Controlfreak707/GM-Copy-Raw', '91a235e09bbb3168c67bc535bdab2a03e88b2a98'],
   ['Controlfreak707/GM-Send-Button', '0c326a4c0968a527fbbc776682b730a83b27e628'],
