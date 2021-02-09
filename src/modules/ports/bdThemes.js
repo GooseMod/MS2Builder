96a6315161a510d3075a55b06d8f00bb1ac6313f
Oj <oj@oojmed.com>
2021-02-09 21:31:27 +0000

    [Chore] Add preview GIF image to Pop! theme port

--- src/modules/ports/bdThemes.js
+++ src/modules/ports/bdThemes.js
@@ -1,4 +1,4 @@
 export default [
   ['LuckFire/Discord-Revamp', 'a42d833c5fb4bc798e3c41e5f4f1e39c34a18cc9', '/discordrevamp.theme.css', 'bdTheme'],
-  ['devinkid1/Pop', '86e2a5f9ccbbaa7151fa97a1b014cff42b756d9f', '/Pop.theme.css', 'bdTheme', { authors: ['418219211043897344'] }]
+  ['devinkid1/Pop', '86e2a5f9ccbbaa7151fa97a1b014cff42b756d9f', '/Pop.theme.css', 'bdTheme', { authors: ['418219211043897344'], images: ['https://media.discordapp.net/attachments/756146058924392545/808810958188314645/gm_pop.gif'] }]
 ];
