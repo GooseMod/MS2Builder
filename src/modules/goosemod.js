ad163c3b393c2d9a120900c7a250215f6230ba6f
Oj <oj@oojmed.com>
2021-02-17 22:30:53 +0000

    [Update Module] Controlfreak707/GM-Better-Animations

--- src/modules/goosemod.js
+++ src/modules/goosemod.js
@@ -6,7 +6,7 @@ export default
   ['Controlfreak707/GM-Copy-Raw', '91a235e09bbb3168c67bc535bdab2a03e88b2a98'],
   ['Controlfreak707/GM-Send-Button', '0c326a4c0968a527fbbc776682b730a83b27e628'],
   ['Controlfreak707/GM-Relaunch', '75c838c352743becdef0d68a954cb5d4f43bce5b'],
-  ['Controlfreak707/GM-Better-Animations', '1f31495d2cbd7b0b53faa39741be89ae4dd37c17'],
+  ['Controlfreak707/GM-Better-Animations', '501ec6031ec2e559f1e3fce89237c043f6120c63'],
   ['Controlfreak707/GM-Comfy-Theme', '27102741d21f94896a9c7ce42614a68612132b8b'],
 
   ['TechnoJo4/GM-MessageClickActions', '9af8d898c23b9b762b87e0fc612a6af0021e383c'],
