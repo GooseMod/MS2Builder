b36c3d37f86265646523d288762d8fa317728c02
Oj <oj@oojmed.com>
2021-02-24 18:19:17 +0000

    [Add Modules] Cynosphere/hjonk (3 submodules)

--- src/modules/goosemod.js
+++ src/modules/goosemod.js
@@ -3,6 +3,10 @@ export default
   // ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d'],
   // ['GooseMod/GMExampleModule', '9f7b1656845746a9913687e675584780537c3f6d', '/subModule'],
 
+  ['Cynosphere/hjonk', '72f63fd08f204b60bb3ba25f66836b9447f623ed', '/createEmoji'],
+  ['Cynosphere/hjonk', '72f63fd08f204b60bb3ba25f66836b9447f623ed', '/oldQuote'],
+  ['Cynosphere/hjonk', '72f63fd08f204b60bb3ba25f66836b9447f623ed', '/timeBarAllActivities'],
+
   ['Controlfreak707/GM-Copy-Raw', '91a235e09bbb3168c67bc535bdab2a03e88b2a98'],
   ['Controlfreak707/GM-Send-Button', '0c326a4c0968a527fbbc776682b730a83b27e628'],
   ['Controlfreak707/GM-Relaunch', '75c838c352743becdef0d68a954cb5d4f43bce5b'],
