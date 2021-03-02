bfde42a2b7e902ed55471927c1437635ef7450af
Oj <oj@oojmed.com>
2021-03-02 14:19:52 +0000

    [Chore] Move around modules between MS2Porter, BD and PC themes

--- src/modules/ports/bdThemes.js
+++ src/modules/ports/bdThemes.js
@@ -25,12 +25,14 @@ export default [
     authors: ['359175647257690113']
   }],
 
-  ['Discord-Theme-Addons/bottom-bar', '', '/src/support/BottomBar.theme.css', 'bdTheme', {
-    authors: ['399416615742996480']
+  ['DiscordStyles/Slate', '', '/Slate.theme.css', 'bdTheme', {
+    authors: ['254362351170617345', '174868361040232448'],
+    images: ['https://i.imgur.com/GEcQZhN.png']
   }],
 
-  ['Discord-Theme-Addons/gradientbuttons-v2', '', '/src/support/GradientButtons.theme.css', 'bdTheme', {
-    authors: ['399416615742996480']
+  ['A-User-s-Discord-Themes/Couve', '', '/download/betterdiscord/couve.theme.css', 'bdTheme', {
+    authors: ['264062457448759296'],
+    images: ['https://cdn.discordapp.com/attachments/539180316447997974/725737819015872562/unknown.png']
   }],
 
   /* ['Pavui/Nebula', '', '/nebula.theme.css', 'bdTheme', {
--- src/modules/ports/pcThemes.js
+++ src/modules/ports/pcThemes.js
@@ -6,5 +6,22 @@ export default [
 
   ['CircuitRCAY/Duvbox', '', '/powercord_manifest.json', 'pcTheme', {
     authors: ['255114091360681986']
-  }]
+  }],
+
+  ['j0lol/i-fixed-discord', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ['363437829507776513']
+  }],
+
+  ['Discord-Theme-Addons/bottom-bar', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ['399416615742996480']
+  }],
+
+  ['Discord-Theme-Addons/gradientbuttons-v2', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ['399416615742996480']
+  }],
+
+  ['Discord-Theme-Addons/modern-channel-indicators', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ['399416615742996480'],
+    images: ['https://raw.githubusercontent.com/Discord-Theme-Addons/modern-channel-indicators/main/assets/recording.gif']
+  }],
 ];
