895a61f6a9f517f3f5d65870222178eeffa67d91
Oj <oj@oojmed.com>
2021-03-05 20:38:02 +0000

    [Core] Add base dev option

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ if (process.argv[2] === '-f') {
   resetDir(modulesDir);
 }
 
+if (process.argv[2] === '-d') {
+  import('./dev.js');
+  process.exit();
+}
+
 let previous = [];
 if (existsSync(clonesDir)) {
   for (const cloneDir of glob.sync(`${clonesDir}/*/*`)) {
