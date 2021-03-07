d3a342705e0f8df46b610a5ab32ec63ac56ec48a
Oj <oj@oojmed.com>
2021-03-07 13:41:37 +0000

    [Autotag] Remove temporary patch for bug in GM v7.0.0

--- src/autoTag.js
+++ src/autoTag.js
@@ -1,6 +1,5 @@
 const basicIncludes = (bundleCode, code, display = (code[0].toUpperCase() + code.substring(1))) => {
-  // if (bundleCode.includes(code)) return display;
-  if (bundleCode.includes(code)) return display.toLowerCase(); // We make the actual tag all lowercase for now due to bug in GM v7.0.0
+  if (bundleCode.includes(code)) return display;
 };
 
 export default (bundleCode) => { // Scans final bundle JS to try and auto-detect things, then adding tags
