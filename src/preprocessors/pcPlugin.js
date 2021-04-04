1cadc61ddab3ca43c0ce26e5591a1f73492db88f
Oj <oj@oojmed.com>
2021-04-04 15:52:41 +0100

    [PCPlugin] Add preprocessor

---
+++ src/preprocessors/pcPlugin.js
@@ -0,0 +1,26 @@
+import { readFileSync, writeFileSync, mkdirSync, rmSync } from 'fs';
+
+export default (manifestPath, repo) => {
+  const pcManifest = JSON.parse(readFileSync(manifestPath, 'utf8'));
+
+  let manifest = {
+    main: 'index.js',
+    tags: ['port'],
+
+    name: pcManifest.name,
+    description: pcManifest.description,
+
+    version: pcManifest.version,
+    authors: [ pcManifest.author ]
+  };
+
+  rmSync(manifestPath);
+  mkdirSync(manifestPath);
+
+  const content = readFileSync(pcManifest.main || 'index.js', 'utf8');//.replace(/\\/g, '\\\\').replace(/`/g, '\\`');
+
+  const jsCode = content;
+
+  writeFileSync(`${manifestPath}/goosemodModule.json`, JSON.stringify(manifest));
+  writeFileSync(`${manifestPath}/index.js`, jsCode);
+};
