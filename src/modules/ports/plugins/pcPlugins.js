c04e34984e1cf9099a6fa1986fa5296d439d7201
Oj <oj@oojmed.com>
2021-04-04 21:31:53 +0100

    [Add Plugin (PCPlugin)] amitpalbadhan/LaTeX-Cord

--- src/modules/ports/plugins/pcPlugins.js
+++ src/modules/ports/plugins/pcPlugins.js
@@ -1,5 +1,9 @@
 export default [
   ['RazerMoon/muteNewGuild', 'a8ea528e67089ea00a457623cebbef6a670127fc', '/manifest.json', 'pcPlugin', {
     authors: ['162970149857656832']
+  }],
+
+  ['amitpalbadhan/LaTeX-Cord', 'e56ae2019d9af9022ea4b278ab2b34ee61880789', '/manifest.json', 'pcPlugin', {
+    authors: ['510591410362122252']
   }]
 ];
