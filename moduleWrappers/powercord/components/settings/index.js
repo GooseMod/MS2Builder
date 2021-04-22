f836b1d517da82939d776017086ed77ffae76c50
Oj <oj@oojmed.com>
2021-04-22 21:43:28 +0100

    [PCCompat > Components > Settings] Reexport SliderInput

--- moduleWrappers/powercord/components/settings/index.js
+++ moduleWrappers/powercord/components/settings/index.js
@@ -1,5 +1,6 @@
 export { default as FormItem } from './formItem';
 export { default as TextInput } from './textInput';
+export { default as SliderInput } from './sliderInput';
 export { default as Divider } from './divider';
 
 export const SwitchItem = goosemodScope.webpackModules.findByDisplayName('SwitchItem');
