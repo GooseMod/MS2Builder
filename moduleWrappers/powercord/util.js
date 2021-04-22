50dc994875a7a0b61a3f5c97c19976f60be6f0a7
Oj <oj@oojmed.com>
2021-04-22 21:02:11 +0100

    [PCCompat > Util] Add sleep, waitFor

--- moduleWrappers/powercord/util.js
+++ moduleWrappers/powercord/util.js
@@ -1,3 +1,16 @@
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 module.exports = {
+  sleep,
+
+  waitFor: async (query) => {
+    while (true) {
+      const el = document.querySelector(query);
+      if (el) return el;
+
+      await sleep(5);
+    }
+  },
+
   ...goosemodScope.reactUtils // Export GooseMod React utils
 };
