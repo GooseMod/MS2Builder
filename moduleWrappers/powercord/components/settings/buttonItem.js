3e0130edefde7c1577fa01ce99bb4b0f4db9bbe8
Oj <oj@oojmed.com>
2021-04-23 17:28:46 +0100

    [PCCompat > Components > Settings] Add ButtonItem

---
+++ moduleWrappers/powercord/components/settings/buttonItem.js
@@ -0,0 +1,36 @@
+const { React } = goosemodScope.webpackModules.common;
+
+import FormItem from './formItem';
+
+const OriginalButton = goosemod.webpackModules.findByProps('Sizes', 'Colors', 'Looks', 'DropdownSizes');
+
+const Tooltip = goosemodScope.webpackModules.findByDisplayName('Tooltip');
+
+
+export default class TextInput extends React.PureComponent {
+  render() {
+    const title = this.props.children;
+    delete this.props.children;
+
+    return React.createElement(FormItem, {
+        title,
+        note: this.props.note,
+        required: this.props.required
+      },
+
+      React.createElement(Tooltip, {
+        text: this.props.tooltipText,
+        position: this.props.tooltipPosition,
+        shouldShow: this.props.tooltipText !== ''
+      }, () => React.createElement(OriginalButton,
+        {
+          color: this.props.success ? OriginalButton.Colors.GREEN : (this.props.color || Buttons.Colors.BRAND),
+          disabled: this.props.disabled,
+          onClick: () => this.props.onClick(),
+          style: { marginLeft: 5 }
+        },
+        this.props.button
+      ))
+    );
+  }
+}
--- moduleWrappers/powercord/components/settings/index.js
+++ moduleWrappers/powercord/components/settings/index.js
@@ -2,5 +2,6 @@ export { default as FormItem } from './formItem';
 export { default as TextInput } from './textInput';
 export { default as SliderInput } from './sliderInput';
 export { default as Divider } from './divider';
+export { default as ButtonItem } from './buttonItem';
 
 export const SwitchItem = goosemodScope.webpackModules.findByDisplayName('SwitchItem');
