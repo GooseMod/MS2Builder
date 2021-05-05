d68d8b286f704baa014aed63d14b47c05b1fd3e9
Oj <oj@oojmed.com>
2021-05-05 18:18:07 +0100

    [Scripts > Add] Add BD theme support

--- scripts/add.js
+++ scripts/add.js
@@ -12,25 +12,25 @@ const rl = createInterface({
 });
 
 
-const [ type1, type2, repo ] = process.argv.slice(2);
+const [ type1, type2, repo, subpath ] = process.argv.slice(2);
 
-console.log(type1, type2, repo);
+console.log(type1, type2, repo, subpath);
+
+const ask = async (prompt) => {
+  return await new Promise((resp) => {
+    rl.question(prompt, (ans) => {
+      resp(ans);
+    });
+  });
+};
 
 const repeatAsk = async (prompt) => {
   const answers = [];
 
-  const ask = async () => {
-    return await new Promise((resp) => {
-      rl.question(prompt, (ans) => {
-        resp(ans);
-      });
-    });
-  };
-
   let ans;
 
   while (ans !== '') {
-    ans = await ask();
+    ans = await ask(prompt);
     if (ans === '') break;
 
     answers.push(ans);
@@ -61,6 +61,16 @@ switch (type1) {
     images: ${JSON.stringify(images)}
   }],`;
     
+        break;
+      }
+
+      case 'bd': {
+        file = `src/modules/ports/themes/bdThemes.js`;
+        js = `  ['${repo}', '', '${subpath}', 'bdTheme', {
+    authors: ${JSON.stringify(authors)},
+    images: ${JSON.stringify(images)}
+  }],`;
+
         break;
       }
     }
@@ -94,7 +104,7 @@ if (shouldCommit) {
 
   await exec(`git add dist ${file}`);
 
-  await exec(`git commit -m "[(Auto) Add Theme (PCTheme)] ${repo}"`);
+  await exec(`git commit -m "[(Auto) Add Theme (${type2.toUpperCase()}Theme)] ${repo}"`);
 
   console.log('Pushing...');
 
