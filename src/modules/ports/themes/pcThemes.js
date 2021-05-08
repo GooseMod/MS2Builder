64d13bd2158704a1c357b4ac4a81a13c4fe1bfb4
Oj <oj@oojmed.com>
2021-05-08 21:09:38 +0100

    [(Auto) Add Theme (PCTheme)] NYRI4/Discolored

--- src/modules/ports/themes/pcThemes.js
+++ src/modules/ports/themes/pcThemes.js
@@ -168,4 +168,9 @@ export default [
     authors: ["681203159158489243"],
     images: ["https://github.com/JayWilson7/BorderShapes/raw/master/Previews/Preview_1.png","https://github.com/JayWilson7/BorderShapes/raw/master/Previews/Preview_2.png","https://github.com/JayWilson7/BorderShapes/raw/master/Previews/Preview_3.png"]
   }],
+
+  ['NYRI4/Discolored', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ["265924886461939712"],
+    images: ["https://nyri4.github.io/Discolored/assets/screenshot.png"]
+  }],
 ];
