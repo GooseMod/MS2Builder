2146e7c514b8f51f4c7638c18d96c3a749bf450b
Oj <oj@oojmed.com>
2021-05-19 21:13:26 +0100

    [Add Plugin (GM)] GooseMod-Modules/CustomCSS

--- src/modules/goosemod.js
+++ src/modules/goosemod.js
@@ -17,6 +17,7 @@ export default
   ['TechnoJo4/GM-MessageClickActions', '3dbadf674f451d5d115aa9cb842172415d91ccbc'],
 
   ['GooseMod-Modules/CopyAvatarURL', '8c02c9b5dabc04f8a41cbfa519e58c00b1895a23'],
+  ['GooseMod-Modules/CustomCSS', '64969856598a2cc2980988046e0ff266d64fa943'],
 
   ['mugman174/togoosestore', '4b3f9c355eac9abeace174a261519ab992135ff3', '/mock'],
   ['mugman174/togoosestore', '4b3f9c355eac9abeace174a261519ab992135ff3', '/rpc'],
