2a18c6f25236119ac28978c5cc912378c10a9411
Oj <oj@oojmed.com>
2021-06-18 19:00:23 +0100

    [(Auto) Add Theme (PCTheme)] CorellanStoma/CreArts

--- src/modules/ports/themes/pcThemes.js
+++ src/modules/ports/themes/pcThemes.js
@@ -257,4 +257,9 @@ export default [
     authors: ["224524143482241035"],
     images: ["https://media.discordapp.net/attachments/828218219927306261/853635731581960192/Ovel_v1.0.0.png","https://i.imgur.com/3BMUuND.png","https://i.imgur.com/csW8EoK.png","https://i.imgur.com/Otv0JjS.png"]
   }],
+
+  ['CorellanStoma/CreArts', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ["288362986991648778"],
+    images: ["https://user-images.githubusercontent.com/58918358/122546250-0fad7b80-d02f-11eb-9b92-df4268d5fc98.png","https://user-images.githubusercontent.com/58918358/122546272-14722f80-d02f-11eb-9025-f6f351d24fbe.png","https://user-images.githubusercontent.com/58918358/122546285-16d48980-d02f-11eb-86a7-0eee638561d3.png"]
+  }],
 ];
