bdedfea55f7ff2f7f85cbb89def62ddc47140dc7
Oj <oj@oojmed.com>
2021-06-26 16:56:09 +0100

    [(Auto) Add Theme (BDTheme)] monstrousdev/themes

--- src/modules/ports/themes/bdThemes.js
+++ src/modules/ports/themes/bdThemes.js
@@ -146,4 +146,9 @@ export default [
     authors: ["402272736665272320"],
     images: ["https://i.imgur.com/sLnMnBp.png","https://i.imgur.com/s41A0Ux.png","https://i.imgur.com/L8JamP6.png"]
   }],
+
+  ['monstrousdev/themes', '', '/theme-files/Tranquil.theme.css', 'bdTheme', {
+    authors: ["402272736665272320"],
+    images: ["https://i.imgur.com/4EvvDc8.png","https://i.imgur.com/b0l0uir.png","https://i.imgur.com/O8gNXd9.png"]
+  }],
 ];
