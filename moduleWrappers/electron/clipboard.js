1174a3f0bff966fa2f058a6da72ff87b6e72ee1b
Oj <oj@oojmed.com>
2021-07-20 20:56:11 +0100

    [ElCompat] Use Webpack module for clipboard copy

--- moduleWrappers/electron/clipboard.js
+++ moduleWrappers/electron/clipboard.js
@@ -1,7 +1,9 @@
+const { copy } = goosemod.webpackModules.findByProps('SUPPORTS_COPY', 'copy'); // Use Webpack module for Web support (instead of DiscordNative)
+
 export const writeText = (text) => { // Write text to clipboard
-  return DiscordNative.clipboard.copy(text);
+  return copy(text);
 };
 
 export const readText = () => { // Read text from clipboard
-  return DiscordNative.clipboard.read();
+  return DiscordNative.clipboard.read(); // Web doesn't support clipboard reading (no Webpack for it like copy) - use DiscordNative
 };
